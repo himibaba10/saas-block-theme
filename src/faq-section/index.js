391aeba462ba21f4f6b6ac0cce0c7032af0f2a09
Ferdous Ahmed <himibaba10@gmail.com>
2026-08-10 02:20:37 +0600

    feat: created index.js of faq-section

---
+++ src/faq-section/index.js
@@ -0,0 +1,11 @@
+import { registerBlockType } from '@wordpress/blocks';
+
+import metadata from './block.json';
+import Edit from './edit';
+import './editor.css';
+import save from './save';
+
+registerBlockType(metadata.name, {
+  edit: Edit,
+  save,
+});
